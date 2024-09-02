d5073bdb9858db6e348302d7ab3e08abea84b047
Roberto Garcia <jorogarverri@gmail.com>
2024-09-02 14:30:53 -0700

    Initialization backend for delivery app

---
+++ server.js
@@ -0,0 +1,35 @@
+const express =  require('express')
+const app =  express();
+const http = require('http');
+const server  =  http.createServer(app);
+const logger  =  require('morgan');
+const cors =  require('cors');
+
+const port = process.env.PORT || 3000;
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended : true
+}));
+app.use(cors());
+app.disable('x-powered-by');
+
+app.set('port', port);
+
+server.listen(3000, '192.168.1.8' || 'localhost', function(){
+    console.log('Application de nodeJS ' +  process.pid + ' Iniciada... en puerto ' + port);
+}); //puerto que va escuchar el servidor
+
+app.get('/', (req, res)=>{
+    res.send('Ruta raiz del backend')
+})
+
+app.get('/test', (req, res)=>{
+    res.send('Este es la ruta de TEST')
+})
+
+app.use((err, req, res, next)=>{
+    console.log(err);
+    res.status(err.status || 500).send(err.stack);
+});
