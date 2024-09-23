cb8f671d11a9f998c2d63ea19f41b167d0996d85
Roberto Garcia <jorogarverri@gmail.com>
2024-09-22 22:22:35 -0600

    Creating scripts to insert roles into database

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,16 @@
+DROP TABLE IF EXISTS roles CASCADE;
+CREATE TABLE roles(
+	id BIGSERIAL PRIMARY KEY,
+	name varchar (100) NOT NULL UNIQUE,
+	image VARCHAR (255) NULL,  
+	route VARCHAR (255) NULL,
+	created_at TIMESTAMP(0) NOT NULL,
+	updated_at TIMESTAMP(0) NOT NULL
+
+);
+
+
+DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users(
 	id BIGSERIAL PRIMARY KEY, 
 	email VARCHAR(255) NOT NULL UNIQUE, 
@@ -12,6 +25,20 @@ CREATE TABLE users(
 	updated_at TIMESTAMP(0) NOT NULL
 	
 );
+
+DROP TABLE IF EXISTS user_has_roles CASCADE;
+CREATE TABLE user_has_roles(
+	id_user BIGSERIAL NOT NULL,
+	id_rol BIGSERIAL NOT NULL,
+	created_at TIMESTAMP(0) NOT NULL,
+	updated_at TIMESTAMP(0) NOT NULL,
+	FOREIGN KEY(id_user) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY(id_rol)  REFERENCES roles(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	PRIMARY KEY (id_user, id_rol)
+
+);
+
+
 /* Insercion*/
 insert into users(
 	email,
@@ -31,3 +58,46 @@ VALUES (
 		'2024-09-02',
 		'2024-09-02'
 )
+
+/*INSERTANDO ROLES*/
+INSERT INTO roles(
+	name, 
+	route,
+	image,
+	created_at,
+	updated_at
+)VALUES(
+	'CLIENTE',
+	'client/home',
+	'https://cdn-icons-png.flaticon.com/512/9187/9187604.png',
+	'2024-09-20',
+	'2024-09-21'
+);
+
+INSERT INTO roles(
+	name, 
+	route,
+	image,
+	created_at,
+	updated_at
+)VALUES(
+	'RESTAURANTE',
+	'restaurant/home',
+	'https://cdn-icons-png.flaticon.com/512/433/433087.png',
+	'2024-09-20',
+	'2024-09-21'
+);
+
+INSERT INTO roles(
+	name, 
+	route,
+	image,
+	created_at,
+	updated_at
+)VALUES(
+	'REPARTIDOR',
+	'delivery/home',
+	'https://cdn-icons-png.flaticon.com/512/2830/2830312.png',
+	'2024-09-20',
+	'2024-09-21'
+);
